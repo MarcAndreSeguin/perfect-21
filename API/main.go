e708f49738439ae72da16f2289bcd515f7b775fc
Marc-Andre Seguin <marcandre.seguin@hey.com>
2025-09-24 14:39:55 -0400

    More CORS origins on for Vercel access

--- API/main.go
+++ API/main.go
@@ -265,6 +265,7 @@ func main() {
 			"http://localhost:5173",          // local dev
 			"https://perfect-21.vercel.app/", // add prod origin
 			"https://perfect-21-lz9spkr3n-marc-andre-seguins-projects.vercel.app",
+			"https://perfect-21-3kopy35d9-marc-andre-seguins-projects.vercel.app",
 		},
 		AllowMethods:     []string{"GET", "HEAD", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
